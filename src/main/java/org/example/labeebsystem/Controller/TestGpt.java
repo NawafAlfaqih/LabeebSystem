a1dc8ea10a3fd6631cc6102c188629a71d6cd16e
Abdulrahman <alshahrani996655@gmail.com>
2025-12-08 23:31:09 +0300

    add: Open AI service

---
+++ src/main/java/org/example/labeebsystem/Controller/TestGpt.java
@@ -0,0 +1,23 @@
+package org.example.labeebsystem.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.labeebsystem.API.ApiResponse;
+import org.example.labeebsystem.Service.ChatGPTService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/gpt")
+@RequiredArgsConstructor
+public class TestGpt {
+    private final ChatGPTService chatGPTService;
+
+    @PostMapping("/test/{ask}")
+    public ResponseEntity<?> test(@PathVariable String ask){
+        String response = chatGPTService.askChatGPT(ask);
+        return ResponseEntity.status(200).body(new ApiResponse(response));
+    }
+}
---
+++ src/main/java/org/example/labeebsystem/Service/ChatGPTService.java
@@ -0,0 +1,25 @@
+package org.example.labeebsystem.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatGPTService {
+    private final OpenAiChatModel chatModel;
+
+    public String askChatGPT(String promptText) {
+
+        try {
+            UserMessage message = new UserMessage(promptText);
+            Prompt prompt = new Prompt(message);
+            return chatModel.call(prompt).getResult().getOutput().getText();
+
+        } catch (Exception e) {
+            return "AI Error: " + e.getMessage();
+        }
+    }
+}
