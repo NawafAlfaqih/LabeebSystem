dc1dc0e0adbf6b16f61235cbfafd333196075e2c
safcsp <dev@test.com>
2025-12-08 20:53:59 +0300

    parent model

---
+++ src/main/java/org/example/labeebsystem/Model/Parent.java
@@ -0,0 +1,30 @@
+package org.example.labeebsystem.Model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+public class Parent {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String Email;
+
+    private Integer password;
+
+   private Double balance;
+}
---
+++ src/main/java/org/example/labeebsystem/Repository/ParentRepository.java
@@ -0,0 +1,7 @@
+package org.example.labeebsystem.Repository;
+
+import org.example.labeebsystem.Model.Parent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParentRepository extends JpaRepository<Parent,Integer> {
+}
