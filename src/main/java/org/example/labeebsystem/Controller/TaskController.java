738e876b1ab45657a65f193516c07282f8ef99bd
Nawaf Alfaqih <nawafalfaqih@outlook.com>
2025-12-09 16:54:56 +0300

    Add: Task Controller

---
+++ src/main/java/org/example/labeebsystem/Controller/TaskController.java
@@ -0,0 +1,51 @@
+package org.example.labeebsystem.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.labeebsystem.Model.Task;
+import org.example.labeebsystem.Service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/task")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @GetMapping("/get/{adminId}")
+    public ResponseEntity<?> getAllTasks(@PathVariable Integer adminId) {
+        return ResponseEntity.status(200).body(taskService.getAllTasks(adminId));
+    }
+    @PostMapping("/add/teacher/{studentId}/{teacherId}")
+    public ResponseEntity<?> addTeacherTask(@RequestBody Task task, @PathVariable Integer studentId,
+                                            @PathVariable Integer teacherId) {
+        taskService.addTeacherTask(task, studentId, teacherId);
+        return ResponseEntity.status(200).body("Task added by teacher successfully");
+    }
+    @PostMapping("/add/parent/{studentId}/{teacherId}/{parentId}")
+    public ResponseEntity<?> addParentTask(@RequestBody Task task, @PathVariable Integer studentId,
+            @PathVariable Integer teacherId, @PathVariable Integer parentId) {
+        taskService.addParentTask(task, studentId, teacherId, parentId);
+        return ResponseEntity.status(200).body("Task added by parent successfully");
+    }
+
+    @PutMapping("/update/{taskId}/{teacherId}")
+    public ResponseEntity<?> updateTask(@PathVariable Integer taskId, @PathVariable Integer teacherId,
+                                        @RequestBody Task task) {
+        taskService.updateTask(taskId, task, teacherId);
+        return ResponseEntity.status(200).body("Task updated successfully");
+    }
+
+    @DeleteMapping("/delete/teacher/{taskId}/{teacherId}")
+    public ResponseEntity<?> deleteTeacherTask(@PathVariable Integer taskId, @PathVariable Integer teacherId) {
+        taskService.deleteTeacherTask(taskId, teacherId);
+        return ResponseEntity.status(200).body("Task deleted by teacher successfully");
+    }
+
+    @DeleteMapping("/delete/parent/{taskId}/{parentId}")
+    public ResponseEntity<?> deleteParentTask(@PathVariable Integer taskId, @PathVariable Integer parentId) {
+        taskService.deleteParentTask(taskId, parentId);
+        return ResponseEntity.status(200).body("Task deleted by parent successfully");
+    }
+}
